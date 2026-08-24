b4da5910bae2c1379fc9b20898d1649aa4d9de2c
4ndr3wyu-byte <4ndr3wyu@gmail.com>
2026-08-24 21:07:14 +1000

    Revise service worker for cache management
    Updated cache version and added activation event to clean up old caches.

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,24 @@
-const CACHE_NAME = 'c2t-player-v1';
+const CACHE_NAME = 'c2t-player-v2';
 const ASSETS = [
-  './cplayer.html',
+  './',
+  './index.html',
   './manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
