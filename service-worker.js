d4574f2b0aa097ed2ad1d1e21b257a9f465eb4b6
4ndr3wyu-byte <4ndr3wyu@gmail.com>
2026-08-24 20:48:31 +1000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'c2t-player-v1';
+const ASSETS = [
+  './cplayer.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
